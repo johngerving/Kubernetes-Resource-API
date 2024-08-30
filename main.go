30d293343231bbae7e5251263fa338b15b0391b3
jhg51 <jhg51@humboldt.edu>
2024-08-30 11:14:58 -0700

    Initial commit; added getNodeInfo function for getting node capacity and allocatable resources

---
+++ main.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+type Resources struct {
+	Cpu       resource.Quantity
+	Memory    resource.Quantity
+	Gpu       resource.Quantity
+	Ephemeral resource.Quantity
+}
+
+type Node struct {
+	Name        string
+	Taints      []corev1.Taint
+	Allocatable Resources
+	Capacity    Resources
+	Free        Resources
+}
+
+type NodeResources struct {
+	Allocatable Resources
+	Requests    map[corev1.ResourceName]resource.Quantity
+	Limits      map[corev1.ResourceName]resource.Quantity
+}
+
+func main() {
+
+	// Get arguments after program name
+	args := os.Args[1:]
+
+	// Exit with error if kubeconfig path not provided
+	if len(args) == 0 {
+		fmt.Println("error: expected kubeconfig path")
+		os.Exit(1)
+	}
+
+	// Get pointer to first argument after program name
+	kubeconfig := &args[0]
+
+	// Create a config from the kubeconfig file
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Create a Kubernetes clientset
+	clientset, err := kubernetes.NewForConfig(config)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	nodes := make(map[string]*Node)
+
+	getNodeInfo(clientset, nodes)
+}
+
+// getNodeInfo modifies a map of nodes, adding entries with the node name as a key.
+// It gets the name of the node, its taints, capacity, and allocatable resources. These are added to the nodes map.
+func getNodeInfo(client kubernetes.Interface, nodes map[string]*Node) error {
+	// Get all nodes in the cluster
+	nodeList, err := client.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
+
+	if err != nil {
+		return err
+	}
+
+	// Loop through the nodes
+	for _, node := range nodeList.Items {
+		// Get the GPU capacity of the node - default 0
+		gpuCapacity := node.Status.Capacity["nvidia.com/gpu"]
+
+		// Loop through the fields of the node capacity
+		for key, value := range node.Status.Capacity {
+			// If the node is a GPU node, set the gpuCapacity to its GPU count
+			if strings.HasPrefix(key.String(), "nvidia.com") && !value.IsZero() {
+				gpuCapacity = value
+			}
+		}
+
+		// Create a new Node with the correct resources
+		newNode := Node{
+			Name:   node.Name,
+			Taints: node.Spec.Taints,
+			Capacity: Resources{
+				Cpu:       node.Status.Capacity.Cpu().DeepCopy(),
+				Memory:    node.Status.Capacity.Memory().DeepCopy(),
+				Gpu:       gpuCapacity,
+				Ephemeral: node.Status.Capacity.StorageEphemeral().DeepCopy(),
+			},
+			Allocatable: Resources{
+				Cpu:       node.Status.Allocatable.Cpu().DeepCopy(),
+				Memory:    node.Status.Allocatable.Memory().DeepCopy(),
+				Gpu:       gpuCapacity,
+				Ephemeral: node.Status.Allocatable.StorageEphemeral().DeepCopy(),
+			},
+		}
+
+		nodes[node.Name] = &newNode
+	}
+
+	for _, node := range nodes {
+		bytes, _ := json.MarshalIndent(node, "\t", "\t")
+		fmt.Println(string(bytes))
+	}
+
+	return nil
+}

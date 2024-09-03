6bb4ba93588de3ef36592e47d2aa8ed25bdab147
jhg51 <jhg51@humboldt.edu>
2024-09-03 11:57:54 -0700

    Added test for getNodeFreeResources

--- main_test.go
+++ main_test.go
@@ -10,6 +10,8 @@ import (
 	"k8s.io/client-go/kubernetes/fake"
 )
 
+// TestGetNodeInfo calls getNodeInfo on a map[string]*Nodes, checking that the resources in the resulting map
+// match the mock nodes' resource values.
 func TestGetNodeInfo(t *testing.T) {
 	// Create a fake Kubernetes client
 	kubeClient := fake.NewClientset()
@@ -100,5 +102,141 @@ func TestGetNodeInfo(t *testing.T) {
 				t.Fatalf(`nodes[%v].Allocatable.Gpu = %v, want match for %v`, node.Name, nodes[node.Name].Allocatable.Gpu.Value(), gpuAllocatable.Value())
 			}
 		}
+	}
+}
+
+// TestGetNodeFreeResources calls getNodeFreeResources on a map[string]*Nodes, checking that the free resources in
+// the resulting map match the mock nodes' values.
+func TestGetNodeFreeResources(t *testing.T) {
+	// Create a fake Kubernetes client
+	kubeClient := fake.NewClientset()
+
+	// Create two fake nodes, node-1 and node-2
+	newNodes := []v1.Node{
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "node-1",
+			},
+			Status: v1.NodeStatus{
+				Capacity: v1.ResourceList{
+					v1.ResourceCPU:              *resource.NewMilliQuantity(24000, resource.DecimalSI),
+					v1.ResourceMemory:           *resource.NewMilliQuantity(5000, resource.DecimalSI),
+					v1.ResourceEphemeralStorage: *resource.NewMilliQuantity(32000, resource.DecimalSI),
+				},
+				Allocatable: v1.ResourceList{
+					v1.ResourceCPU:              *resource.NewMilliQuantity(20000, resource.DecimalSI),
+					v1.ResourceMemory:           *resource.NewMilliQuantity(4000, resource.DecimalSI),
+					v1.ResourceEphemeralStorage: *resource.NewMilliQuantity(28000, resource.DecimalSI),
+				},
+			},
+		},
+		{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: "node-2",
+			},
+			Status: v1.NodeStatus{
+				Capacity: v1.ResourceList{
+					v1.ResourceCPU:              *resource.NewMilliQuantity(16000, resource.DecimalSI),
+					v1.ResourceMemory:           *resource.NewMilliQuantity(16000, resource.DecimalSI),
+					"nvidia.com/gpu":            *resource.NewQuantity(2, resource.DecimalSI),
+					v1.ResourceEphemeralStorage: *resource.NewMilliQuantity(32000, resource.DecimalSI),
+				},
+				Allocatable: v1.ResourceList{
+					v1.ResourceCPU:              *resource.NewMilliQuantity(12000, resource.DecimalSI),
+					v1.ResourceMemory:           *resource.NewMilliQuantity(12000, resource.DecimalSI),
+					"nvidia.com/gpu":            *resource.NewQuantity(2, resource.DecimalSI),
+					v1.ResourceEphemeralStorage: *resource.NewMilliQuantity(28000, resource.DecimalSI),
+				},
+			},
+		},
+	}
+
+	// Define two pods to be added to the cluster
+	pod1 := &v1.Pod{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Pod",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "pod-1",
+		},
+		Spec: v1.PodSpec{
+			Containers: []v1.Container{
+				{
+					Name:  "ubuntu",
+					Image: "ubuntu",
+					Resources: v1.ResourceRequirements{
+						Requests: v1.ResourceList{
+							v1.ResourceCPU:              *resource.NewQuantity(4, resource.DecimalSI),
+							v1.ResourceMemory:           *resource.NewQuantity(2, resource.DecimalSI),
+							v1.ResourceEphemeralStorage: *resource.NewMilliQuantity(4000, resource.DecimalSI),
+							"nvidia.com/gpu":            *resource.NewQuantity(1, resource.DecimalSI),
+						},
+					},
+				},
+			},
+			NodeName: "node-2",
+		},
+	}
+	pod2 := &v1.Pod{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Pod",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "pod-2",
+		},
+		Spec: v1.PodSpec{
+			Containers: []v1.Container{
+				{
+					Name:  "ubuntu",
+					Image: "ubuntu",
+					Resources: v1.ResourceRequirements{
+						Requests: v1.ResourceList{
+							v1.ResourceCPU:              *resource.NewQuantity(3, resource.DecimalSI),
+							v1.ResourceMemory:           *resource.NewQuantity(5, resource.DecimalSI),
+							v1.ResourceEphemeralStorage: *resource.NewMilliQuantity(8500, resource.DecimalSI),
+						},
+					},
+				},
+			},
+			NodeName: "node-2",
+		},
+	}
+
+	// Create the nodes with the fake client
+	for _, node := range newNodes {
+		kubeClient.CoreV1().Nodes().Create(context.TODO(), &node, metav1.CreateOptions{})
+	}
+
+	// Create the two pods on the cluster
+	kubeClient.CoreV1().Pods("default").Create(context.TODO(), pod1, metav1.CreateOptions{})
+	kubeClient.CoreV1().Pods("default").Create(context.TODO(), pod2, metav1.CreateOptions{})
+
+	// Create a map[string]*Node to store the resources and requests
+	nodes := make(map[string]*Node)
+	// Get the capacity and allocatable for each node
+	getNodeInfo(kubeClient, nodes)
+	// Get the pod requests and subtract from the allocatable to get the free resources
+	getNodeFreeResources(kubeClient, nodes)
+
+	switch {
+	// Test free resources for node-1 - should be equal to allocatable resources since no pods are on the node
+	case !nodes["node-1"].Free.Cpu.Equal(*resource.NewQuantity(20, resource.DecimalSI)):
+		t.Fatalf(`nodes[%v].Free.Cpu = %v, want match for %v`, "node-1", &nodes["node-1"].Free.Cpu, resource.NewQuantity(20, resource.DecimalSI).Value())
+	case !nodes["node-1"].Free.Memory.Equal(*resource.NewMilliQuantity(4000, resource.DecimalSI)):
+		t.Fatalf(`nodes[%v].Free.Memory = %v, want match for %v`, "node-1", &nodes["node-1"].Free.Memory, resource.NewMilliQuantity(4000, resource.DecimalSI).Value())
+	case !nodes["node-1"].Free.Ephemeral.Equal(*resource.NewMilliQuantity(28000, resource.DecimalSI)):
+		t.Fatalf(`nodes[%v].Free.Ephemeral = %v, want match for %v`, "node-1", &nodes["node-1"].Free.Ephemeral, resource.NewMilliQuantity(28000, resource.DecimalSI).Value())
+
+	// Test free resources for node-2
+	case !nodes["node-2"].Free.Cpu.Equal(*resource.NewQuantity(5, resource.DecimalSI)):
+		t.Fatalf(`nodes[%v].Free.Cpu = %v, want match for %v`, "node-2", &nodes["node-2"].Free.Cpu, resource.NewQuantity(5, resource.DecimalSI).Value())
+	case !nodes["node-2"].Free.Memory.Equal(*resource.NewMilliQuantity(5000, resource.DecimalSI)):
+		t.Fatalf(`nodes[%v].Free.Memory = %v, want match for %v`, "node-2", &nodes["node-2"].Free.Memory, resource.NewMilliQuantity(5000, resource.DecimalSI).Value())
+	case !nodes["node-2"].Free.Ephemeral.Equal(*resource.NewMilliQuantity(15500, resource.DecimalSI)):
+		t.Fatalf(`nodes[%v].Free.Ephemeral = %v, want match for %v`, "node-2", &nodes["node-2"].Free.Ephemeral, resource.NewMilliQuantity(15500, resource.DecimalSI).Value())
+	case !nodes["node-2"].Free.Gpu.Equal(*resource.NewQuantity(1, resource.DecimalSI)):
+		t.Fatalf(`nodes[%v].Free.Gpu = %v, want match for %v`, "node-2", &nodes["node-2"].Free.Gpu, resource.NewQuantity(1, resource.DecimalSI).Value())
 	}
 }
